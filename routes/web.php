b007f43691827c327ccf81ad3fc5953e0effebcf
Saya Atchibay <190103410@stu.sdu.edu.kz>
2021-02-22 20:07:29 +0500

    create two routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +27,19 @@ Route::get('/services', function () {
 Route::get('/contacts', function () {
     return view('contacts');
 }) -> name('contacts');
+
+Route::get('/post/create', function() {
+    DB::table('posts')->insert([
+        'id' => 1,
+        'title' => 'Lab 4',
+        'body' => 'This is my first post.'
+    ]); 
+});
+
+Route::get('/post', function() {
+    $postTitle = Post::find(1)->title;
+    $postBody = Post::find(1)->body;
+    return $postTitle." <br>".$postBody;
+});
+
+
